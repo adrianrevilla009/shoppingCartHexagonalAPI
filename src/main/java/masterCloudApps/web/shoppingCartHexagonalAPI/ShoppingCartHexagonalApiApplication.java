67b5947508811049000db86c939b8f3bad6ef5e5
adrianrevilla009 <adrianrg1996@gmail.com>
2023-01-19 17:40:04 +0100

    proyect setup

---
+++ src/main/java/masterCloudApps/web/shoppingCartHexagonalAPI/ShoppingCartHexagonalApiApplication.java
@@ -0,0 +1,13 @@
+package masterCloudApps.web.shoppingCartHexagonalAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartHexagonalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartHexagonalApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/masterCloudApps/web/shoppingCartHexagonalAPI/ShoppingCartHexagonalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package masterCloudApps.web.shoppingCartHexagonalAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartHexagonalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
